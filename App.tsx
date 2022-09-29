ebc39f8baa252433ca77631b6a8e3476d6d8a6c4
MonikaHoy <77454575+MonikaHoy@users.noreply.github.com>
2022-09-29 22:17:21 +0200

    add start screen

--- App.tsx
+++ App.tsx
@@ -1,43 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import StartScreen from './src/screens/StartScreen';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Grow</Text>
-        <Text style={styles.sectionDescription}>
-          a picture a day keeps sad plants at bay
-        </Text>
-      </View>
+      <StartScreen />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  section: {
-    justifyContent: 'center',
-    alignItems: 'center',
     flex: 1,
   },
 });
