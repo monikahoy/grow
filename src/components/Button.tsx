e9cb83746a2b5384b34083490d78116e9f504427
MonikaHoy <77454575+MonikaHoy@users.noreply.github.com>
2022-10-03 20:49:10 +0200

    add button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,15 +4,23 @@ import LinearGradient from 'react-native-linear-gradient';
 
 interface ButtonProps {
   title: string;
+  disabled: boolean;
+  onPress: () => void;
 }
 
-const Button = ({title}: ButtonProps) => {
+const Button = ({title, disabled, onPress}: ButtonProps) => {
+  const underLayColor = 'transparent';
+  const colorsDisabled = ['#C8C8C8', '#C8C8C8'];
+  const colorsActive = ['#88C0D0', '#5DC5C2'];
+  const gradientStart = {x: 0, y: 0};
+  const gradientEnd = {x: 1, y: 0};
+
   return (
-    <TouchableHighlight>
+    <TouchableHighlight onPress={onPress} underlayColor={underLayColor}>
       <LinearGradient
-        colors={['#88C0D0', '#5DC5C2']}
-        start={{x: 0, y: 0}}
-        end={{x: 1, y: 0}}
+        colors={disabled ? colorsDisabled : colorsActive}
+        start={gradientStart}
+        end={gradientEnd}
         style={styles.container}>
         <Text style={styles.text}>{title}</Text>
       </LinearGradient>
