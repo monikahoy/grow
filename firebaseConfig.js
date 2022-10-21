9821e6410f00f85a4809575af35a72bea56fd501
MonikaHoy <77454575+MonikaHoy@users.noreply.github.com>
2022-10-21 19:17:54 +0200

    enable sign up with firebase auth

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from 'firebase/app';
-import {getAnalytics} from 'firebase/analytics';
-import {firebaseAuth} from 'firebase/auth';
-import {firebaseFirestore} from 'firebase/firestore';
-import {firebaseStorage} from 'firebase/storage';
+import {getAuth} from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,7 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-export const auth = firebaseAuth();
-export const firestore = firebaseFirestore();
-export const storage = firebaseStorage();
+export const auth = getAuth(app);
