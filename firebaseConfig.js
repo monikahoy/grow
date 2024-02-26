9dfed892e27b613831ae1ea2499220a0f04172a7
MonikaHoy <77454575+MonikaHoy@users.noreply.github.com>
2024-02-26 15:22:18 +0100

    update firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,14 @@
 // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import {initializeApp} from 'firebase/app';
-import {getAuth} from 'firebase/auth';
-import {firebaseStorage} from 'firebase/storage';
-import {firebaseStore} from 'firebase/firestore';
+import {
+  initializeAuth,
+  getReactNativePersistence,
+  getAuth,
+} from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import {getStorage} from 'firebase/storage';
+import {getFirestore} from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,6 +27,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const storage = firebaseStorage();
-export const firestore = firebaseStore();
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+export const storage = getStorage();
+export const firestore = getFirestore();
