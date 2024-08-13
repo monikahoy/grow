ed510d6aa2085ac520176c6bf8f9b0f5ad945229
Monika Høy <monika.hoey@gmail.com>
2024-08-13 18:08:23 +0200

    show loadingscreen until app is loaded and authorised

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, SafeAreaView, StyleSheet, View} from 'react-native';
 import SignedInStack from './src/navigation/SignedInStack';
 import SignedOutStack from './src/navigation/SignedOutStack';
 import {NavigationContainer} from '@react-navigation/native';
@@ -7,17 +7,28 @@ import {auth} from './firebaseConfig';
 import {onAuthStateChanged} from 'firebase/auth';
 import Colors from './src/theme/Colors';
 import BootSplash from 'react-native-bootsplash';
+import LoadingView from './src/components/LoadingView';
 
 const App = () => {
   const [signedIn, setSignedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  onAuthStateChanged(auth, user => {
-    if (user) {
-      setSignedIn(true);
-    } else {
-      setSignedIn(false);
-    }
-  });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user) {
+        setSignedIn(true);
+      } else {
+        setSignedIn(false);
+      }
+      setIsLoading(false); // Set loading to false once auth check is complete
+    });
+
+    return () => unsubscribe(); // Clean up the listener on unmount
+  }, []);
+
+  if (isLoading) {
+    return <LoadingView />;
+  }
 
   return (
     <NavigationContainer
