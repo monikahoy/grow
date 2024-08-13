8cd9104fb07a508df2ab63b511ad9ff654db8782
Monika Høy <monika.hoey@gmail.com>
2024-08-13 18:12:06 +0200

    clean up PlantsList and PlantItem

--- src/components/PlantItem.tsx
+++ src/components/PlantItem.tsx
@@ -1,52 +1,39 @@
-import React from 'react';
-import {View, Text, StyleSheet, Image, Alert} from 'react-native';
-import {TouchableHighlight} from 'react-native';
+import React, {memo} from 'react';
+import {View, Text, StyleSheet, Image, TouchableHighlight} from 'react-native';
 import Colors from '../theme/Colors';
 import Fonts from '../theme/Fonts';
-import {formatDate, getUserId} from '../../utils';
-import {Timestamp} from 'firebase/firestore';
+import {getUserId} from '../utils/utils';
 
-type PlantItem = {
+type PlantItemProps = {
   name: string;
-  timestamp: Timestamp;
   image: string;
   onPress: () => void;
 };
 
-export const PlantItem = ({name, timestamp, image, onPress}: PlantItem) => {
+const PlantItem = memo(({name, image, onPress}: PlantItemProps) => {
   const userId = getUserId();
   if (!userId) {
-    // handle error
-    return;
+    return null;
   }
 
   return (
     <TouchableHighlight onPress={onPress} underlayColor="transparent">
       <View style={styles.container}>
+        <Text style={styles.title}>{name}</Text>
         <Image source={{uri: image}} style={styles.image} />
-        <View style={styles.detailsContainer}>
-          <Text style={styles.title}>{name}</Text>
-          <Text style={styles.date}>{formatDate(timestamp.toDate())}</Text>
-        </View>
       </View>
     </TouchableHighlight>
   );
-};
+});
 
 export default PlantItem;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 20,
-    backgroundColor: Colors.background,
-  },
-  detailsContainer: {
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    flex: 1,
+    padding: 10,
   },
   title: {
     color: Colors.basicText,
@@ -54,15 +41,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginBottom: 10,
   },
-  date: {
-    color: Colors.secondaryText,
-    fontFamily: Fonts.subtitleFont,
-    fontSize: 18,
-  },
   image: {
-    height: 142,
-    width: 107,
+    width: '100%',
+    aspectRatio: 3 / 4, // 3:4 aspect ratio
     borderRadius: 5,
-    marginRight: 20,
   },
 });
